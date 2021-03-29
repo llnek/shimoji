9b80ef02358c088810df08bbb5139146c62152fc
Kenneth Leung <ccllnekk@gmail.com>
2021-03-29 01:40:22 -0700

    updated tacman

--- src/czlab/tacman/tacman.js
+++ src/czlab/tacman/tacman.js
@@ -1,207 +1,170 @@
+/* Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright © 2020-2021, Kenneth Leung. All rights reserved. */
+
 ;(function(window){
+
   "use strict";
 
   function scenes(Mojo){
-    let _Z=Mojo.Scenes,_S=Mojo.Sprites,_I=Mojo.Input,_=Mojo.u;
-    let _T=Mojo.Tiles;
+    const {Scenes:_Z,
+           Sprites:_S,
+           Tiles:_T,
+           Input:_I,
+           "2d":_2d,
+           Game:_G,
+           ute:_,is,EventBus}=Mojo;
+    const MFL=Math.floor;
 
-    function _collide(scene,p){
-      let wall=scene.world.tiled.getTileLayer("Collision",1);
-      for(let w, i=0;i<wall.children.length;++i){
-        w=wall.children[i];
-        Mojo["2d"].hit(p,w);
+    const E_PLAYER=1;
+    const E_ENEMY=2;
+    const E_COIN=4;
+    const E_TOWER=8;
+
+    Mojo.defMixin("enemyAI", function(e){
+      e.m5.direction=Mojo.LEFT;
+      //e.m5.speed=100;
+      e.m5.switchPercent=2;
+      function tryDir(){
+        if(e.m5.vel[1] !== 0 && e.m5.vel[0]=== 0){
+          e.m5.direction = _.rand() < 0.5 ? Mojo.LEFT : Mojo.RIGHT;
+        }else if(e.m5.vel[0] !== 0 && e.m5.vel[1]=== 0){
+          e.m5.direction = _.rand() < 0.5 ? Mojo.UP : Mojo.DOWN;
+        }
       }
-    }
-
-    Mojo.defMixin("enemyControls", function(e){
-      e.mojoh5.direction=Mojo.LEFT;
-      e.mojoh5.switchPercent=2;
+      function changeDir(col){
+        if(e.m5.vel[0]=== 0 && e.m5.vel[1]=== 0){
+          let c=col.overlapN;
+          if(c[1] !== 0){
+            e.m5.direction = _.rand() < 0.5 ? Mojo.LEFT : Mojo.RIGHT;
+          }else if(c[0] !== 0){
+            e.m5.direction = _.rand() < 0.5 ? Mojo.UP : Mojo.DOWN;
+          }
+        }
+      }
       let self={
-        motion(dt){
-          if(Math.random() < e.mojoh5.switchPercent/100){
-            self.tryDirection();
-          }
-          switch(e.mojoh5.direction){
-            case Mojo.LEFT: e.mojoh5.vel[0] = -e.mojoh5.speed; break;
-            case Mojo.RIGHT: e.mojoh5.vel[0] = e.mojoh5.speed; break;
-            case Mojo.UP:   e.mojoh5.vel[1] = -e.mojoh5.speed; break;
-            case Mojo.DOWN: e.mojoh5.vel[1] = e.mojoh5.speed; break;
-          }
+        dispose(){
+          EventBus.unsub(["hit",e],changeDir)
         },
-        tryDirection(){
-          let from = e.mojoh5.direction;
-          if(e.mojoh5.vel[1] !== 0 && e.mojoh5.vel[0]=== 0){
-            e.mojoh5.direction = Math.random() < 0.5 ? Mojo.LEFT : Mojo.RIGHT;
-          }else if(e.mojoh5.vel[0] !== 0 && e.mojoh5.vel[1]=== 0){
-            e.mojoh5.direction = Math.random() < 0.5 ? Mojo.UP : Mojo.DOWN;
+        onTick(dt){
+          if(_.rand() < e.m5.switchPercent/100){
+            tryDir()
           }
-        },
-        changeDirection(col){
-          if(e.mojoh5.vel[0]=== 0 && e.mojoh5.vel[1]=== 0){
-            let c=col.overlapN;
-            if(c[1] !== 0){
-              e.mojoh5.direction = Math.random() < 0.5 ? Mojo.LEFT : Mojo.RIGHT;
-            }else if(c[0] !== 0){
-              e.mojoh5.direction = Math.random() < 0.5 ? Mojo.UP : Mojo.DOWN;
-            }
+          switch(e.m5.direction){
+            case Mojo.LEFT: e.m5.vel[0] = -e.m5.speed; break;
+            case Mojo.RIGHT: e.m5.vel[0] = e.m5.speed; break;
+            case Mojo.UP:   e.m5.vel[1] = -e.m5.speed; break;
+            case Mojo.DOWN: e.m5.vel[1] = e.m5.speed; break;
           }
         }
       };
-      //Mojo.EventBus.sub(["post.step",e],"step",self);
-      Mojo.EventBus.sub(["hit",e],"changeDirection",self);
+      EventBus.sub(["hit",e],changeDir);
       return self;
     });
 
-    function Player(scene,col,row){
-      let p= _S.sprite(_S.frame("sprites.png",32,32,0,0));
-      let K=scene.world.tiled.getScaleFactor();
-      p.y=row*scene.world.tiled.tileW*K+p.height/2;
-      p.x=col*scene.world.tiled.tileH*K+p.width/2;
-      p.scale.x=K;
-      p.scale.y=K;
-      p.anchor.set(0.5);
-      p.mojoh5.speed=150 *K;
-      p.mojoh5.uuid="player";
-      Mojo.addMixin(p,"2d","towerManControls");
-      p.mojoh5.step=function(dt){
-        p["2d"].motion(dt);
-        p["towerManControls"].motion(dt);
+    function Tower(scene,t,ts,ps){
+      t.m5.type=E_TOWER;
+      t.m5.sensor=true;
+      t.m5.onSensor=(colObj)=>{
+        scene.removeTile(t)
       };
-      p.mojoh5.collide=()=>_collide(scene,p);
-      p.mojoh5.resizeMe=function(px,py,pw,ph){
-        let K=scene.world.tiled.getScaleFactor();
-        p.mojoh5.speed=150 *K;
-        p.scale.x=K;
-        p.scale.y=K;
-        p.x= p.x * scene.world.width/pw;
-        p.y= p.y * scene.world.height/ph;
+      t.m5.dispose=()=>{
+        EventBus.unsub(["2d.sensor",t],"onSensor",t.m5)
       };
-      return scene.player=p;
+      EventBus.sub(["2d.sensor",t],"onSensor",t.m5);
+      return t;
     }
 
-    Mojo.defMixin("towerManControls", function(e){
-      let self={
-        motion(dt){
-          if(e.mojoh5.vel[0] > 0){
-            e.angle = 90;
-          }else if(e.mojoh5.vel[0] < 0){
-            e.angle = -90;
-          }else if(e.mojoh5.vel[1] > 0){
-            e.angle = 180;
-          }else if(e.mojoh5.vel[1] < 0){
-            e.angle = 0;
-          }
-          // grab a direction from the input
-          e.mojoh5.direction = _I.keyDown(_I.keyLEFT)  ? Mojo.LEFT :
-                               _I.keyDown(_I.keyRIGHT) ? Mojo.RIGHT :
-                               _I.keyDown(_I.keyUP) ? Mojo.UP :
-                               _I.keyDown(_I.keyDOWN) ? Mojo.DOWN : e.mojoh5.direction;
-          switch(e.mojoh5.direction) {
-            case Mojo.LEFT: e.mojoh5.vel[0] = -e.mojoh5.speed; break;
-            case Mojo.RIGHT: e.mojoh5.vel[0] = e.mojoh5.speed; break;
-            case Mojo.UP:   e.mojoh5.vel[1] = -e.mojoh5.speed; break;
-            case Mojo.DOWN: e.mojoh5.vel[1] = e.mojoh5.speed; break;
-          }
-        }
+    function Dot(scene,s,ts,ps){
+      scene.dotCount = _.or(scene.dotCount,0);
+      scene.dotCount += 1;
+      s.m5.uuid=`dot#${scene.dotCount}`;
+      s.m5.type=E_COIN;
+      s.m5.sensor=true;
+      s.m5.onSensor=(colObj)=>{
+        scene.removeTile(s);
+        scene.dotCount -= 1;
+        if(scene.dotCount===0){}
       };
-      e.mojoh5.direction=Mojo.UP;
-      //e.mojoh5.speed=100;
-      //Mojo.EventBus.sub(["post.step",e],"step",self);
-      return self;
-    });
-
-    function Enemy(scene,id,col,row){
-      let s= _S.sprite(_S.frame("sprites.png",32,32,0,32));
-      let K=scene.world.tiled.getScaleFactor();
-      s.mojoh5.speed= 150*K;
-      s.mojoh5.vel[0]=150;
-      s.mojoh5.vel[1]=150;
-      s.mojoh5.uuid=id;
-      s.scale.x=K;
-      s.scale.y=K;
-      s.y=row*scene.world.tiled.tileW*K+s.height/2;
-      s.x=col*scene.world.tiled.tileH*K+s.width/2;
-      s.mojoh5.boom=function(col){
-        //if(_.inst(Player,col.obj)) { Mojo.runScene("level1"); }
+      s.m5.dispose=()=>{
+        EventBus.unsub(["2d.sensor",s],"onSensor",s.m5)
       };
-      Mojo.EventBus.sub(["bump",s],"boom",s.mojoh5);
-      Mojo.addMixin(s,"2d","enemyControls");
-      s.mojoh5.collide=()=>_collide(scene,s);
-      s.mojoh5.step=function(dt){
-        s["2d"].motion(dt);
-        s["enemyControls"].motion(dt);
-        if(scene.player && Mojo["2d"].hitTest(s,scene.player)){
-          _S.remove(scene.player);
-          scene.player=null;
-        }
-      };
-      s.mojoh5.resizeMe=function(px,py,pw,ph){
-        let K=scene.world.tiled.getScaleFactor();
-        s.mojoh5.speed=150 *K;
-        s.scale.x=K;
-        s.scale.y=K;
-        s.x= s.x * scene.world.width/pw;
-        s.y= s.y * scene.world.height/ph;
-      };
+      EventBus.sub(["2d.sensor",s],"onSensor",s.m5);
       return s;
     }
 
-    _Z.defScene("level1",{
-      setup(){
-        let world=this.world=_T.tiledWorld("map.json");
-        let self=this;
-        world.x=(Mojo.width-world.width)/2;
-        world.y=(Mojo.height-world.height)/2;
-        this.insert(world);
-        this.enemies=[Enemy(this,"e1",10,4), Enemy(this,"e2",15,10), Enemy(this,"e3",5,10)];
-        this.enemies.forEach(e=>world.addChild(e));
-        world.addChild(Player(this,10,7));
-        Mojo.EventBus.sub(["post.update",this],"postUpdate");
-      },
-      onCanvasResize(){
-        let ph=this.world.height;
-        let pw=this.world.width;
-        let px=this.world.x;
-        let py=this.world.y;
-        _S.resize(this);
-        this.world.x=(Mojo.width-this.world.width)/2;
-        this.world.y=(Mojo.height-this.world.height)/2;
-        if(this.player)
-          this.player.mojoh5.resizeMe(px,py,pw,ph);
-        this.enemies.forEach(e=>e.mojoh5.resizeMe(px,py,pw,ph));
-      },
-      postUpdate(){
-        let es=this.world.tiled.getTileLayer("Edibles",1);
-        for(let ps,e,i=0;i<es.children.length;++i){
-          e=es.children[i];
-          ps=e.tiled.props;
-          if(this.player && ps && Mojo["2d"].hitTest(e,this.player)){
-            ps=ps["Class"];
-            if(ps ==="Tower"){
-              _S.remove(e);
-            }
-            if(ps ==="Dot"){
-              _S.remove(e);
-              break;
-            }
-          }
-        }
-      }
-    });
-  }
+    function Player(scene,p,ts,ps,os){
+      p.m5.type=E_PLAYER;
+      p.m5.cmask=E_TOWER | E_COIN;
+      _S.centerAnchor(p);
+      p.x += MFL(p.width/2);
+      p.y += MFL(p.height/2);
+      p.m5.uuid="player";
+      p.m5.speed= 150 * scene.getScaleFactor();
+      _S.velXY(p,p.m5.speed, p.m5.speed);
+      Mojo.addMixin(p,"2d");
+      Mojo.addMixin(p,"2dControls",true);
+      p.m5.tick=function(dt){
+        p["2d"].onTick(dt);
+        p["2dControls"].onTick(dt);
+      };
+      return p;
+    }
 
-  function setup(Mojo){
-    scenes(Mojo);
-    Mojo.Scenes.runScene("level1");
+    function Enemy(scene,s,ts,ps,os){
+      s.m5.uuid=`e#${_.nextId()}`;
+      s.m5.type=E_ENEMY;
+      s.m5.cmask=E_PLAYER;
+      s.x = os.column * s.width+MFL(s.width/2);
+      s.y = os.row * s.height+MFL(s.height/2);
+      _S.centerAnchor(s);
+      s.m5.speed= 150 * scene.getScaleFactor();
+      _S.velXY(s,s.m5.speed, s.m5.speed);
+      Mojo.addMixin(s,"2d");
+      Mojo.addMixin(s,"enemyAI");
+      s.m5.boom=function(col){
+        if(col.B.m5.uuid=="player"){
+          Mojo.pause();
+        }
+      };
+      s.m5.tick=function(dt){
+        s["2d"].onTick(dt);
+        s["enemyAI"].onTick(dt);
+      };
+      EventBus.sub(["bump",s],"boom",s.m5);
+      return s;
+    }
+
+    function _objFactory(scene){
+      return{ Player, Enemy, Dot, Tower }
+    }
+
+    _Z.defScene("level1",{
+      setup(options){
+      }
+    },{sgridX:128,sgridY:128,centerStage:true,
+       tiled:{name: "map.json",factory:_objFactory}});
   }
 
   window.addEventListener("load",()=>{
     MojoH5({
       assetFiles: ["sprites.png", "map.json","tiles.png"],
-      arena: {width:640,height:448},
+      arena: {width:640,height:480},
       scaleToWindow:"max",
-      backgroundColor: 0,
-      start:setup
+      start(Mojo){
+        scenes(Mojo);
+        Mojo.Scenes.runScene("level1");
+      }
     })
   });
 
